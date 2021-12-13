6ebb9044c179ebdd15566bb312fa3200ce066209
Samuel Evans-Powell <mail@sevanspowell.net>
2021-12-13 10:50:43 +0800

    Fix issue on M1 Macs
    - Compile pandoc explicitly with ghc8107 to overcome bug in process package on
    M1 macs:
      Error running filter pandoc-fignos:
      pandoc-fignos: createProcess: runInteractiveProcess: exec: resource exhausted (Cannot allocate memory)

--- shell.nix
+++ shell.nix
@@ -6,7 +6,7 @@ pkgs.mkShell {
   # Explanation of buildInputs vs nativeBuildInputs:
   # https://discourse.nixos.org/t/use-buildinputs-or-nativebuildinputs-for-nix-shell/8464/2
   nativeBuildInputs = with pkgs; [
-    pandoc
+    haskell.packages.ghc8107.pandoc
     python38Packages.pandoc-xnos
     python38Packages.pandoc-fignos
     python38Packages.pandoc-eqnos
