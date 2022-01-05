4013765e8630ad4c03add492a0c9434516a67048
Samuel Evans-Powell <mail@sevanspowell.net>
2022-01-05 11:05:14 +0800

    GitHub actions workflow for pull requests

--- default.nix
+++ default.nix
@@ -8,4 +8,13 @@
     inherit (pkgs.haskell.packages.ghc8107) pandoc;
     inherit (pkgs.python38Packages) pandoc-xnos pandoc-fignos pandoc-eqnos pandoc-tablenos manubot;
   };
+
+  acp-site-tarball = pkgs.runCommand "acp-site-tarball" {}
+  ''
+    mkdir -p $out
+    tar -czvf $out/acp-site.tar.gz -C ${acp-site}/share/doc/acp --preserve-permissions .
+
+    mkdir -p $out/nix-support/
+    echo "file tarball $out/acp-site.tar.gz" >> $out/nix-support/hydra-build-products
+  '';
 }
